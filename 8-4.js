f00e63fdc9b0ec86bfc9c108f7a6b8d615e7bddc
tomsloni <aiki.waw@gmail.com>
2016-11-24 23:28:37 +0100

    adding some details

---
+++ 8-4.js
@@ -0,0 +1,11 @@
+var a = prompt('Enter a'), 
+	h = prompt('Enter h'), 
+	triangleArea = a*h/2;
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+document.write('Pole trójkąta o podstawie a: ' + h + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea)
+
+document.write('Zadanie 8-4')
+
+
