e7b72f7803c8699c4bcbf63fa81232543b115804
tomsloni <aiki.waw@gmail.com>
2016-11-25 10:59:27 +0100

    adding task 8.6

---
+++ zadanie_8_6/zadanie_8_6.js
@@ -0,0 +1,20 @@
+var lenght = prompt('Podaj długość boku: '), 
+	height = prompt('Podaj wysokość: ');
+
+function getTriangleArea(a, h) {
+	if ((a > 0) && (h > 0)) {
+		return (a*h/2);
+	} else {
+		return 'Nieprawidłowe dane';
+	}
+}
+
+document.write('Pole Twojego trójkąta: ' + getTriangleArea(lenght, height));
+
+var triangle1Area = getTriangleArea(1, 5);
+var triangle2Area = getTriangleArea(7, 3);
+var triangle3Area = getTriangleArea(1434, 5232);
+
+document.write(' Pole 1: ' + triangle1Area);
+document.write(' Pole 2: ' + triangle2Area);
+document.write(' Pole 3: ' + triangle3Area);
