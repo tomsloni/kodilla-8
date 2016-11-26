09543a57fe5b61843c6657f96e4809952a839285
Michał Dolny <michal.dolny@getresponse.com>
2016-11-26 09:24:30 +0100

    wyczyszczenie mastera

--- triangleArea/8-4.js
+++
@@ -1,9 +0,0 @@
-var a = prompt('Enter a'), 
-	h = prompt('Enter h'), 
-	triangleArea = a*h/2;
-
-console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
-
-document.write('Pole trójkąta o podstawie a: ' + h + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea)
-
-
