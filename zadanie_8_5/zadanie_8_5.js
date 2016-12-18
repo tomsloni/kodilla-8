fc744746b0a8ba630fb32c49488784ece328b395
tomsloni <aiki.waw@gmail.com>
2016-12-18 16:29:44 +0100

    changes applied

--- zadanie_8_5/zadanie_8_5.js
+++ zadanie_8_5/zadanie_8_5.js
@@ -2,13 +2,23 @@ var a = prompt("Podaj liczbę a: ", 0),
 	b = prompt("Podaj liczbę b: ", 0),
 	value = (a*a) + (2*a*b) - (b*b);
 
-if (value > 0) {
-	console.log('wynik dodatni');
-	document.write('wynik dodatni');
-} else if(value == 0) {
-	console.log('wynik zerowy');
-	document.write('wynik zerowy');
+if (isNaN(value)) {
+	console.log('Wynik nieokreślony.');
+	document.write('Wynik nieokreślony.');
 } else {
-	console.log('wynik dodatni');
-	document.write('wynik ujemny');
+	if (value > 0) {
+		console.log('Wynik dodatni. ');
+		document.write('Wynik dodatni. ');
+	} else if(value == 0) {
+		console.log('Wynik zerowy. ');
+		document.write('Wynik zerowy. ');
+	} else {
+		console.log('Wynik ujemny. ');
+		document.write('Wynik ujemny. ');
+	}
+	console.log('Otrzymana liczba to: ' + value);
+	document.write('Otrzymana liczba to: ' + value);
 }
+
+
+
